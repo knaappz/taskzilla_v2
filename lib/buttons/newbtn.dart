4160c2099bee1cca13938b0e628282243cf50d86
Jakub Knapik <128633057+Knaappz@users.noreply.github.com>
2024-01-17 10:29:12 +0100

    Bottom navigation bar and button for adding tasks.

---
+++ lib/buttons/newbtn.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class AddBTN extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const AddBTN({Key? key, required this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: SizedBox(
+        width: 80,
+        height: 80,
+        child: FloatingActionButton(
+          elevation: 0,
+          onPressed: onPressed,
+          child: ClipOval(
+            child: Image.asset(
+              'assets/img/zilla.png',
+              width: 70,
+              height: 70,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
